ec8f03206dd6aa638c40a2efcbddf61473eb6adb
Cristina Homobono Fernandez <chomobon@c1r13s4.42madrid.com>
2025-07-29 14:58:58 +0200

    De momento pone todas las paredes, tengo que limpiar un poco el sprite y poner el resto de cosas

--- put_sprites.c
+++ put_sprites.c
@@ -6,7 +6,7 @@
 /*   By: chomobon <chomobon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 13:49:10 by chomobon          #+#    #+#             */
-/*   Updated: 2025/07/29 14:11:37 by chomobon         ###   ########.fr       */
+/*   Updated: 2025/07/29 14:58:07 by chomobon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ void put_wall(t_map game)
     int j;
 
     i = 0;
-    j = 0;
     game.wall_img = mlx_xpm_file_to_image(game.mlx, WALL, &game.wall_w, &game.wall_h);
-    while(game.map[i][j])
+    while(game.map[i])
     {
+        j = 0;
         while(game.map[i][j])
         {
             if (game.map[i][j] == '1')
                 mlx_put_image_to_window(game.mlx, game.mlx_wind, game.wall_img,
-                    game.wall_w, game.wall_h);
+                    j * 56, i * 56);
             j++;
         }
         i++;
